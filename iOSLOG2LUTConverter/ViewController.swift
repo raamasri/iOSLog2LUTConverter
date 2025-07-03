5f85ef457f2e558ae005046dd5976ef7c1585666
raama srivatsan <raama@me.com>
2025-07-03 10:18:22 -0700

    Initial Commit

---
+++ iOSLOG2LUTConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSLOG2LUTConverter
+//
+//  Created by raama srivatsan on 7/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSLOG2LUTConverterTests/iOSLOG2LUTConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSLOG2LUTConverterTests.swift
+//  iOSLOG2LUTConverterTests
+//
+//  Created by raama srivatsan on 7/3/25.
+//
+
+import Testing
+@testable import iOSLOG2LUTConverter
+
+struct iOSLOG2LUTConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
